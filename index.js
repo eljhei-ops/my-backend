425eeb9c25a84b72dbeea1da426560ca31769992
eljhei-ops <ljregondola21@gmail.com>
2025-12-03 06:53:42 +0800

    nge

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "frontend-test")));
 
 // Serve login.html only for unknown NON-API routes
-app.get("/:path(*)", (req, res, next) => {
+app.use((req, res, next) => {
     if (req.path.startsWith("/api/")) return next();
     res.sendFile(path.join(__dirname, "frontend-test", "login.html"));
 });
